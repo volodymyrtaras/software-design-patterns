b26f7726c8e87755554c58abd1de06181d8c9f3f
vtaras <taras.volodymyr.bogdanovych@gmail.com>
2024-07-07 11:19:12 +0200

    Added description of "Flyweight" pattern

--- src/main/java/vtaras/patterns/structural/flyweight/Forest.java
+++ src/main/java/vtaras/patterns/structural/flyweight/Forest.java
@@ -3,6 +3,11 @@ package vtaras.patterns.structural.flyweight;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+ * The Flyweight pattern minimizes memory usage by sharing common parts of object state between multiple objects, reducing redundancy
+ *
+ * It separates shared state from unique state to optimize resource usage, especially useful in large-scale systems with many similar objects
+ */
 public class Forest {
 
     private final List<Tree> treeList = new ArrayList<>();
@@ -18,7 +23,7 @@ public class Forest {
         List<String> eventList = new ArrayList<>();
 
         for (Tree tree : treeList) {
-          eventList.add(  tree.draw());
+            eventList.add(tree.draw());
         }
 
         return eventList;
